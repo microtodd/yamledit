896b4e080f762a4f12f89cf3d26e9cda0bfa82d6
Todd Davenport <todd@HRT-ME-LO-P-4.local>
2016-10-15 10:27:41 -0500

    Updated to v0.2, and added create function

--- yamledit.py
+++ yamledit.py
@@ -8,12 +8,14 @@ import ruamel.yaml
 from ruamel import yaml
 from ruamel.yaml.scalarstring import SingleQuotedScalarString, DoubleQuotedScalarString
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 # TODO
 #
-# ) add new key/value
 # ) remove a key/value
+# ) support lists
+# ) merge two yaml files capability
+# ) Support input pipe instead of file
 #
 
 ## printHelp
@@ -30,11 +32,26 @@ def printHelp():
     -o <filename>       (Optional), output file, if not specified goes to STDOUT
     -y                  (Optional), if passed then any user confirmation is assumed "yes"
     -q                  (Optional), if passed then everything is silent. This option implies -y.
-    -r <key> <newvalue> 'key' is of format foo.bar.biz.baz
+
+        You must pick one and only one: -r or -c
+
+    -r <key> <newvalue> Replace.  'key' is of format foo.bar.biz.baz
+                        If key does not exist, returns error.
                         If used it must be the last option used.
 
-    Example: python ecyaml.py -f in.yml -o out.yml -f foo.bar.biz baz
-    This will set:
+    Example: python ecyaml.py -f in.yml -o out.yml -r foo.bar.biz baz
+    This will replace:
+        foo:
+            bar:
+                biz: baz
+
+    -c <key> <newvalue> Create. 'key is of format foo.bar.biz.baz.
+                        If key already exists, will prompt to overwrite
+                        unless -y is selected.
+                        If used it must be the last option used.
+
+    Example: python ecyaml.py -f in.yml -o out.yml -c foo.bar.biz baz
+    This will create:
         foo:
             bar:
                 biz: baz
@@ -52,6 +69,7 @@ def printVersion():
 # @param[in] [keyName,newValue]
 # @param[in] autoConfirm
 # @param[in] quiet
+#
 def replaceValue(inputFileName, outputFileName, values, autoConfirm, quiet):
     keyName = values[0]
     newValue = values[1]
@@ -99,6 +117,70 @@ def replaceValue(inputFileName, outputFileName, values, autoConfirm, quiet):
         newFile.write( ruamel.yaml.round_trip_dump(data) )
         newFile.close()
 
+## createValue
+#
+# @param[in] inputFileName
+# @param[in] outputFileName
+# @param[in] [keyName,newValue]
+# @param[in] autoConfirm
+# @param[in] quiet
+#
+def createValue(inputFileName, outputFileName, values, autoConfirm, quiet):
+    keyName = values[0]
+    newValue = values[1]
+    inputFile = None        # Handle to input file data
+
+    # Open file
+    try:
+        inputFile = open(inputFileName)
+    except Exception as e:
+        raise Exception('Could not open/parse file \'' + str(inputFileName) + '\': ' + str(e))
+
+    # Load it
+    data = ruamel.yaml.round_trip_load(inputFile, preserve_quotes=True)
+
+    # See if the key exists, create the new path if necessary
+    keyAlreadyExists = True
+    keyPath = str(keyName).split('.')
+    lastNodeName = keyPath.pop()
+    currentNode = data
+    for nodeName in keyPath:
+        if nodeName in currentNode:
+            currentNode = currentNode[nodeName]
+        else:
+            keyAlreadyExists = False
+            currentNode[nodeName] = {}
+            currentNode = currentNode[nodeName]
+
+    if lastNodeName not in currentNode:
+        currentNode[lastNodeName] = {}
+
+    outputMessage = 'Creating '
+    if keyAlreadyExists:
+        outputMessage = 'Updating existing key '
+
+    if not quiet:
+        if isinstance(currentNode[lastNodeName],str):
+            print outputMessage + '\'' + str(keyName) + '\' from \'' + currentNode[lastNodeName] + '\' to \'' + newValue + '\''
+        else:
+            print outputMessage + '\'' + str(keyName) + '\' as \'' + newValue + '\''
+
+    if autoConfirm == False and quiet == False:
+        userInput = raw_input("Continue? (y/n): ")
+        if userInput != 'y' and userInput != 'Y':
+            print "Aborting."
+            return
+
+    currentNode[lastNodeName] = newValue
+
+    # Output
+    if outputFileName is None:
+        print ruamel.yaml.round_trip_dump(data)
+    else:
+        newFile = open(outputFileName,'w')
+        newFile.write( ruamel.yaml.round_trip_dump(data) )
+        newFile.close()
+
 ## main
 #
 def main(argv):
@@ -111,7 +193,7 @@ def main(argv):
     quiet = False
 
     # Grab and process the command line arguments
-    opts, args = getopt.getopt(argv, 'hvyqf:o:r')
+    opts, args = getopt.getopt(argv, 'hvyqf:o:rc')
     for opt, arg in opts:
         if opt == '-f':
             inputFileName = str(arg)
@@ -147,6 +229,20 @@ def main(argv):
                 newValue = str(args[1])
             actions['replace'] = [sourceKey,newValue]
 
+        # If -c is used, we assume two arguments
+        if opt == '-c':
+            if len(args) != 2:
+                print >> sys.stderr, 'ERROR: -c expects 2 arguments'
+                sys.exit(2)
+
+            sourceKey = None
+            newValue = None
+            if args[0]:
+                sourceKey = str(args[0])
+            if args[1]:
+                newValue = str(args[1])
+            actions['create'] = [sourceKey,newValue]
+
     # Error checking
     if inputFileName is None:
         print >> sys.stderr, 'ERROR: input file name expected (-f option)'
@@ -161,6 +257,13 @@ def main(argv):
         if action == 'replace':
             try:
                 replaceValue(inputFileName,outputFileName,actions[action],autoConfirm,quiet)
+            except Exception as e:
+                print 'ERROR: ' + str(e)
+                sys.exit(5)
+
+        elif action == 'create':
+            try:
+                createValue(inputFileName,outputFileName,actions[action],autoConfirm,quiet)
             except Exception as e:
                 print 'ERROR: ' + str(e)
                 sys.exit(5)
